2f648be1ff43999385878a1498d00b3a23a61fde
le-anne <leannevictoriaharris@gmail.com>
2024-05-20 17:41:16 -0700

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blahg.netlify.app/",
+	site: "l-ea.netlify.app",
 	base: "/",
 	integrations: [sitemap()],
 	markdown: {
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "Leanne the EA";
+export const SITE_DESCRIPTION = "Everything tax. Book now.";
